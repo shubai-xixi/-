ba078fe70ba71bd8eab304a7cc9a44d52e78af93
Deathlingling <1580527185@qq.com>
2023-01-08 18:00:15 +0800

    python爬虫实例

---
+++ 手机转QQ2.0.py
@@ -0,0 +1,32 @@
+import requests
+import json
+import xlwt
+import time
+
+count = 0
+xxx = 0
+
+workbook = xlwt.Workbook()
+sheet = workbook.add_sheet("手QQ")
+while True:
+    qwer = 17542130567
+    var = qwer + xxx
+    url = f"https://zy.xywlapi.cc/qqphone?phone={var}" #接口1
+    res = requests.get(url)
+    text = res.text
+    xxx = xxx + 1
+    print(qwer,text)
+    data = json.loads(text)
+    for each in range(0, 1):
+        if data['status'] == 500:
+            continue
+        else:
+            time.sleep(0.1)
+            sheet.write(count, 0, xxx)
+            sheet.write(count, 1, str(data['status']))
+            sheet.write(count, 2, str(data['message']))
+            sheet.write(count, 3, str(data['qq']))
+            sheet.write(count, 4, str(data['']))
+            sheet.write(count, 5, str(data['phonediqu']))
+            count = count + 1
+    workbook.save('手机转QQ数据.xls')
