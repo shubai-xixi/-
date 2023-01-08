5a9a5c41598c0a4fe3f86b79820930818de7e72c
Deathlingling <1580527185@qq.com>
2023-01-08 19:06:44 +0800

    python爬虫实例

--- QQ转手机，微博，LOL2.0.py
+++ QQ转手机，微博，LOL2.0.py
@@ -17,7 +17,7 @@ while True:
     text = res.text
     qwer=qwer+1
     xxx=xxx+1
-    print(xxx,res.text)
+    print(xxx,qwer,res.text)
     data = json.loads(text)
     for each in range(0, 1):
         if data['status'] == 500:
@@ -26,10 +26,11 @@ while True:
             zxc = zxc + 1
         time.sleep(0.1)
         sheet.write(count, 0, zxc)  # row, column, value
-        sheet.write(count, 1, str(data['phone']))
-        sheet.write(count, 2, str(data['phonediqu']))
-        sheet.write(count, 3, str(data['lol']))
-        sheet.write(count, 4, str(data['wb']))
-        sheet.write(count, 5, str(data['qqlm']))
+        sheet.write(count, 1, qwer)
+        sheet.write(count, 2, str(data['phone']))
+        sheet.write(count, 3, str(data['phonediqu']))
+        sheet.write(count, 4, str(data['lol']))
+        sheet.write(count, 5, str(data['wb']))
+        sheet.write(count, 6, str(data['qqlm']))
         count = count + 1
     workbook.save('QQ转数据.xls')
