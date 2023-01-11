c79c2f1417f56733c87679b458f0450dda008daa
Deathlingling <1580527185@qq.com>
2023-01-11 17:20:11 +0800

    Merge remote-tracking branch 'pa/master'

--- 杂项.py
+++ 杂项.py
@@ -2,7 +2,7 @@ import requests
 
 mubiao = ('https://api.uomg.com/api/visitor.info?skey=1',
           'https://api.uomg.com/api/rand.qinghua?format=json',
-          '',
+          'https://api.vvhan.com/api/song?txt={as}',
           ''
           '')
 
@@ -10,9 +10,10 @@ def asdf(qwer):
     res = requests.get(mubiao[qwer])
     print(res.text)
 
-while True:
-    opq=int(input('ip地址  输入0\n'
-                  '土味情话 输入1\n'
-                  ''
-                  '输入: '))
-    asdf(opq)
+
+opq=int(input('ip地址          输入0\n'
+                '土味情话         输入1\n'
+                '文字转语音        输入2\n'
+                  '                 输入: '))
+
+asdf(opq)
