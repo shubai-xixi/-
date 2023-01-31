4d422f6011c48289c1141c571e01339e768e2797
Deathlingling <1580527185@qq.com>
2023-01-31 20:31:25 +0800

    Merge remote-tracking branch 'pa/master'

--- 小说下载/小说下载1.py
+++ 小说下载/小说下载1.py
@@ -11,14 +11,15 @@ while True:
     res = requests.get(url)
     res.encoding = 'UTF-8'
     print(res)
-    if res.status_code == 404:
-        continue
-    else:
-        qqq=re.findall('<title.*?>(.+?)</title>', res.text)
-        asdf=asdf+1
-        list=qqq[0]
-        with open('%s.html'% list, 'wb') as picture:  # 要读取二进制文件，如图片、视频等，要用'wb'模式写入文件
+    if res.status_code == 200:
+        qqq = re.findall('<title.*?>(.+?)</title>', res.text)
+        asdf = asdf + 1
+        list = qqq[0]
+        with open('%s.html' % list, 'wb') as picture:  # 要读取二进制文件，如图片、视频等，要用'wb'模式写入文件
             picture.write(res.content)
             print('ok')
-            print(qwer - 1, res,'已下载',zxcv,'章')
-            zxcv=zxcv+1
+            print(qwer - 1, res, '已下载', zxcv, '章')
+            zxcv = zxcv + 1
+
+    else:
+        continue
